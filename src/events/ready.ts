40f7ab4d042c27a7efa39e76fe6ddedda33722ad
KibaOfficial <kontakt.lprg@gmail.com>
2024-07-19 19:00:33 +0200

    "changed ready event log text"

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,6 +8,6 @@ import logger from '../utils/logger';
 
 export default function (bot: Bot) {
   bot.on('ready', async () => {
-    logger({ status: "INFO", message: `Ready! Logged in as ${bot.user?.tag}. Hello From ./events/ready.ts`});
+    logger({ status: "INFO", message: `Ready! Logged in as ${bot.user?.tag}.`});
   });
 }
