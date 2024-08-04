ec1a0811e93591c5721862d3d6e212660c8f5ec1
KibaOfficial <kontakt.lprg@gmail.com>
2024-08-04 13:30:18 +0200

    "edited help command"

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -12,25 +12,26 @@ const command: Command = {
     .setName("help")
     .setDescription("Sends helpful information about this bot"),
   async execute(interaction) {
-    const embed =  new EmbedBuilder()
-    .setColor("#00ff00")
-    .setTitle("Willkommen")
-    .setDescription("For more help please visit: https://discord.gg/vRvxbuQUVn")
-    .addFields(
-      { name: 'User Commands', value: '\u200b'},
-      { name: '/ping', value: "Tests the latency to the discord api" },
-      { name: '/help', value: "Sends this message" },
-      { name: '\u200b', value: '\u200b'},
-      { name: 'Adminstrative Commands', value: "\u200b" },
-      { name: '/setrole', value: 'Gives a specific user a defined role' },
-      { name: '/removerole', value: "Removes a specific role from a defined user" },
-      { name: '/setwelcome', value: "Sets the welcome channel for this server" },
-      { name: '\u200b', value: '\u200b'},
-      { name: 'Owner Commands', value: '\u200b'},
-      { name: '/reboot', value: "Reboots the bot" },
-    )
-    .setTimestamp()
-    .setFooter({ text: `Made with ❤️ by KibaOfficial`})
+    const embed = new EmbedBuilder()
+      .setColor("#00ff00")
+      .setTitle("Welcome to our Bot!")
+      .setDescription("Here is a list of available commands and their descriptions.")
+      .addFields(
+        { name: 'User Commands', value: 'Commands that all users can use:' },
+        { name: '/ping', value: "Tests the latency to the Discord API" },
+        { name: '/help', value: "Sends this help message" },
+        { name: '/weather', value: "Get weather data for your city" },
+        { name: '\u200b', value: '\u200b' }, // Empty line for better separation
+        { name: 'Administrative Commands', value: "Commands for administrators:" },
+        { name: '/setrole', value: 'Gives a specific user a defined role' },
+        { name: '/removerole', value: "Removes a specific role from a defined user" },
+        { name: '/setwelcome', value: "Sets the welcome channel for this server" },
+        { name: '\u200b', value: '\u200b' }, // Empty line for better separation
+        { name: 'Owner Commands', value: 'Commands that only the owner can use:' },
+        { name: '/reboot', value: "Reboots the bot" }
+      )
+      .setTimestamp()
+      .setFooter({ text: `Made with ❤️ by KibaOfficial` });
 
     interaction.reply({ embeds: [embed] });
   },
